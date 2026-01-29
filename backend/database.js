77c00c0e0c27042fd7de442534c7ae63aa4a2a07
Abser yousuf <abseryousuf50@gmail.com>
2026-01-29 16:29:06 +0530

    UPDATED DB JS

--- backend/database.js
+++ backend/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config({ path: "./backend/.env" });
 const mongooseUrl = process.env.MONGO_URL;
 const ConnectToMongo = async () => {
   try {
