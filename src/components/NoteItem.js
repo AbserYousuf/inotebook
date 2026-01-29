29d8bd96d1c18514ad9b4dbe3bc15abc30683061
Abser yousuf <abseryousuf50@gmail.com>
2026-01-29 16:00:44 +0530

    UPDATED NOTE ITEM

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,29 +1,25 @@
 import { useContext } from "react";
 import notecontext from "../context/notes/notecontext";
 export const NoteItem = (props) => {
- 
-    const context = useContext(notecontext)
-    const {handleAlert,handleAlert2, deletenotes}= context
-  const { note ,update} = props;
-  const callde =()=>{
-      deletenotes(
-      note._id
-      )
-
-       handleAlert()
-  }
+  const context = useContext(notecontext);
+  const { handleAlert, handleAlert2, deletenotes } = context;
+  const { note, update, noteId } = props;
+  const calldel = () => {
+    console.log(noteId)
+    deletenotes(noteId);
+    handleAlert();
+  };
   return (
     <>
       <div className="card col-md-3 mx-3">
-       
         <div className="card-body ">
-          <h5 className="card-title">{note.title}
-         
-
-          </h5>
+          <h5 className="card-title">{note.title}</h5>
           <p className="card-text">{note.description}</p>
-          <i className="fa-solid fa-trash " onClick={callde}></i>
-          <i className="fa-regular fa-pen-to-square mx-3" onClick={()=>{update(note,handleAlert2)}}></i>
+          <i className="fa-solid fa-trash " onClick={calldel}></i>
+          <i
+            className="fa-regular fa-pen-to-square mx-3"
+            onClick={() => { update(note, handleAlert2) }}
+          ></i>
         </div>
       </div>
     </>
